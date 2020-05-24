7ded45a43a43d53d7379b844cc031c5b825c2d11
shengrongchun <1010449768@qq.com>
2020-05-24 17:29:17 +0800

    pages

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   title: 'Vue Parse',
+  base: '/vue-doc/',
   description: '图文并茂解析vue主要原理',
   markdown: {
     lineNumbers: true
--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
 # Vue Parse
 
 `这是一个 VUE 重点知识点原理讲解项目`
-[链接地址](https://www.baidu.com)
+[链接地址](https://shengrongchun.github.io/vue-doc/)
